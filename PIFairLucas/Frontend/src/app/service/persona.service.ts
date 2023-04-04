1b21d710861162ac0618d1daa2de66ffc5308391
Fair Lucas <lukitaa.27@gmail.com>
2023-04-04 17:20:22 -0300

    typo

--- PIFairLucas/Frontend/src/app/service/persona.service.ts
+++ PIFairLucas/Frontend/src/app/service/persona.service.ts
@@ -7,10 +7,11 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class PersonaService {
-  URL = 'https://localhost:8080/personas/';
+  URL = 'http://localhost:8080/personas/';
+
   constructor(private http: HttpClient) { }
 
   public getPersona(): Observable<persona>{
-    return this.http.get<persona>(this.URL+'traer/perfil');
+    return this.http.get<persona>(this.URL+ 'traer/perfil');
   }
 }
